ca7bb2170d24e5fbc545594acc2ec414de208d44
Yousif Abozid <yousif.abozid@yahoo.com>
2021-03-22 10:11:56 +0200

    add avatar

---
+++ components/Avatar.js
@@ -0,0 +1,9 @@
+const Avatar = () => {
+    return (
+        <div className="avatar">
+            <img src="/avatar.jpg" alt="avatar" />
+        </div>
+    )
+}
+
+export default Avatar
--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import React from "react"
+import Avatar from "./Avatar"
 import NavBar from "./NavBar"
 
 const Layout = ({ children }) => {
     return (
         <div className="row">
             <div className="col-md-4" style={{ border: "1px solid black" }}>
-                picture
+                <Avatar />
+                <div className="bio">
+                    <h2>Yousif Abozid</h2>
+                    <p>Full Stack Web Developer</p>
+                </div>
             </div>
             <div className="col-md-8" style={{ border: "1px solid black" }}>
                 <NavBar />
